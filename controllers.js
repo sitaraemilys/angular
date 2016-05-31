336eff077eebd0699c5080fdd6b4a174fac5593d
sitypop <sityemilyshah@gmail.com>
2016-05-31 15:41:02 +0100

    Initial commit

---
+++ controllers.js
@@ -0,0 +1,16 @@
+//
+// 1. Got the root module (toDoApp), which is equal to angular.module('toDoApp', [])
+// 2. Used the controller method on this root module to attach a controller module
+// 3. Named the controller module ToDoController
+// 4. Set up the controller in a callback provided to the controller() method.
+
+toDoApp.controller('ToDoController', function() {
+  // anything in here is 'controller code'
+  var self = this;
+  self.todos = [{ text: "ToDo1", completed: true }, { text: "ToDo2", completed: false }];
+
+  self.addToDo = function(todoText){
+    self.todos.push( { text: todoText, completed: false });
+  };
+
+});
