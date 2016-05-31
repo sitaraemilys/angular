07ec616fa38565b95205c4364cf4b860ee75d31c
sitypop <sityemilyshah@gmail.com>
2016-05-31 16:46:01 +0100

    ToDoService

---
+++ ToDoService.js
@@ -0,0 +1,24 @@
+// in ToDoService.js
+toDoApp.service('ToDoService', ['$http', 'ToDoFactory', function($http, ToDoFactory) {
+  var self = this;
+
+  self.getAll = function() {
+    var todos = [];
+    _fetchTodosFromApi(todos);
+    return todos;
+  };
+
+  // Why have we not set this private function to self?
+  function _fetchTodosFromApi(todos) {
+    $http.get('http://quiet-beach-24792.herokuapp.com/todos.json')
+      .then(function(response) {
+        _handleResponseFromApi(response.data, todos);
+      }, function(err) {});
+  }
+
+  function _handleResponseFromApi (data, todos) {
+    data.forEach(function(toDoData){
+      todos.push(new ToDoFactory(toDoData.text, toDoData.completed));
+    });
+  }
+}]);
--- controllers.js
+++ controllers.js
@@ -4,10 +4,10 @@
 // 3. Named the controller module ToDoController
 // 4. Set up the controller in a callback provided to the controller() method.
 
-toDoApp.controller('ToDoController', ['ToDoFactory', function(ToDoFactory) {
+toDoApp.controller('ToDoController', ['ToDoService', 'ToDoFactory', function(ToDoService, ToDoFactory) {
   // anything in here is 'controller code'
   var self = this;
-  self.todos = [{ text: "ToDo1", completed: true }, { text: "ToDo2", completed: false }];
+  self.todos = ToDoService.getAll();
 
   self.addToDo = function(todoText){
     self.todos.push(new ToDoFactory(todoText));
